7ceefef4570d98a4cbb5d75916ab1cd28f9608de
damon_estrada <damon.estrada@student.nmt.edu>
2020-01-27 18:32:50 -0700

    Add more tokens.

--- src/TokenType.java
+++ src/TokenType.java
@@ -2,9 +2,11 @@ public enum TokenType {
         TK_PLUSEQ,
         TK_PLUS,
         TK_MINUS,
-        TK_EQUAL,
         TK_SEMICOLON,
         TK_KEYWORDS,
         TK_TYPE,
-        TK_IDENTIFIER
+        TK_IDENTIFIER,
+        TK_NUMBER,
+        TK_EQUALS,
+        TK_SPACE
 }
