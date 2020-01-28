0f63606ae4526a0b849656c139ba57840b0903e2
damon_estrada <damon.estrada@student.nmt.edu>
2020-01-27 18:32:07 -0700

    A more concise way of displaying tokens.

--- src/Token.java
+++ src/Token.java
@@ -4,6 +4,7 @@ public class Token {
         TokenType tokenType;
 
         public String toString() {
-            return str + tokenType.toString();
+
+            return "(" + str + ", " + tokenType + ")";
         }
 }
