560f2f2f587a8dcf209f1901af8429813f78fbac
damon_estrada <damon.estrada@student.nmt.edu>
2020-01-27 18:31:14 -0700

    Sterling and Damon implemented a way to see where invalid token occur (line number, character position).

--- src/Lexer.java
+++ src/Lexer.java
@@ -8,9 +8,9 @@ import static java.lang.System.setOut;
 
 public class Lexer {
 
-    private static LinkedHashMap<Pattern, TokenType> patterns = new LinkedHashMap<Pattern, TokenType>();
+    private static LinkedHashMap<Pattern, TokenType> patterns = new LinkedHashMap<>();
 
-    // regex patterns required to match token types
+    // Regex patterns required to match token types
     static {
         //TODO: braces, brackets, parenthesis, additional types, additional keywords, and special operators
 
@@ -23,64 +23,81 @@ public class Lexer {
         patterns.put(Pattern.compile("^\\+="), TokenType.TK_PLUSEQ);
         patterns.put(Pattern.compile("^\\+"), TokenType.TK_PLUS);
         patterns.put(Pattern.compile("^-"), TokenType.TK_MINUS);
-        patterns.put(Pattern.compile("^\\="), TokenType.TK_EQUAL);
+        patterns.put(Pattern.compile("^[=]"), TokenType.TK_EQUALS);
         patterns.put(Pattern.compile("^;"), TokenType.TK_SEMICOLON);
         patterns.put(Pattern.compile("^(int)|(char)|(void)"), TokenType.TK_TYPE);
         patterns.put(Pattern.compile("^(main)|(return)"), TokenType.TK_KEYWORDS);
         patterns.put(Pattern.compile("^[A-Za-z_][A-Za-z0-9_]*"), TokenType.TK_IDENTIFIER);
+        patterns.put(Pattern.compile("^[0-9]+"), TokenType.TK_NUMBER);
+        patterns.put(Pattern.compile("^\\s"),TokenType.TK_SPACE);
+        // why does -?[0-9]+ work?
     }
 
     /**
      * Tokenizes the lines in the c-file by iterating over the lines,
-     * @param lines
-     * @return
+     * @param lines C syntax
+     * @return tokens of the file on success.
      */
     public static ArrayList<Token> tokenize(String[] lines) {
 
         ArrayList<Token> tokens = new ArrayList<>();
+        int fileLine = 1; // Used to see where in the file failure occurs.
 
         for(String line : lines) {
+
+            /* Keep reference to original string to see which 'token' isn't accepted.*/
+            String originalLine = line;
+
             while(line.length() > 0) {
 
                 int end = 0;
-                // move the end marker past white-space
+                // Move the end marker past white-space
                 while(Character.isWhitespace(line.charAt(end))) end++;
 
-                // checking for comments within /* and */ to ignore
+                // Checking for comments within /* and */ to ignore
                 if(line.charAt(end) == '/' && line.charAt(end + 1) == '*') {
                     end += 2;
 
                     while(line.charAt(end) != '*' && line.charAt(end + 1) != '/') end++;
                 }
 
-                // shorten string to eliminate whitespace and comments up to this point
+                // Shorten string to eliminate whitespace and comments up to this point
                 if(end != 0) {
-                    line = line.substring(end, line.length());
+                    line = line.substring(end);
                 }
 
-                // iterate through regex
+                int counter = 1;
+
+                // Iterate through regex
                 for(Map.Entry<Pattern, TokenType> e : Lexer.patterns.entrySet()) {
 
                     Matcher m = e.getKey().matcher(line);
 
+                    counter += 1;
+
                     if(m.find()) {
                         // if sequence found, add to list of tokens, and shorten the string again
                         Token tk = new Token();
-                        tk.str = line.substring(m.start(), m.end()) + " --> ";
+                        tk.str = line.substring(m.start(), m.end());
                         tk.tokenType = e.getValue();
                         tokens.add(tk);
 
-                        System.out.println(end);
-                        line = line.substring(m.end(), line.length());
-
-                    } else {
-                        // error and exit if there is an unrecognized token.
-                        System.out.println("error: unrecognized token!");
-                        exit(0);
+                        line = line.substring(m.end());
+                        counter = 1;
                     }
 
+                    /* If counter reaches the size of the regex map size, it isn't a supported token. */
+                    if (counter == patterns.size()){
+                        // Error and exit if there is an unrecognized token. Print location of failure.
+                        System.err.println("error: unrecognized token! -- > " + line);
+                        System.out.println("'Token' at position " +
+                                (originalLine.indexOf(line.charAt(0)) + 1) +
+                                " on line " + fileLine);
+                        exit(0);
+                    }
                 }
             }
+            fileLine++; // Next line; ; meaning line was successfully tokenized.
         }
         return tokens;
     }
