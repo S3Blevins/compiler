72f0c34aa434fd856d3f7b6791aac800a619a1f6
damon_estrada <damon.estrada@student.nmt.edu>
2020-01-29 13:57:12 -0700

    Added constructors to aid in testing.

--- src/Token.java
+++ src/Token.java
@@ -7,4 +7,19 @@ public class Token {
 
             return "(" + str + ", " + tokenType + ")";
         }
+
+    /**
+     * Default constructor
+     */
+    public Token(){}
+
+    /**
+     * Constructor to assign fields in class.
+     * @param str The string that corresponds to the token.
+     * @param tokenType the token generated from the string.
+     */
+    public Token(String str, TokenType tokenType) {
+        this.str = str;
+        this.tokenType = tokenType;
+    }
 }
