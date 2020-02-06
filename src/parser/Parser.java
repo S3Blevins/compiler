590175612cfc645f49b1f475f10bfbe8c92cc269
Garrett Bates <garrett.bates@student.nmt.edu>
2020-02-06 12:45:06 -0700

    Added static parse method stub.

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -1,7 +1,17 @@
 package parser;
 
+import java.util.ArrayList;
+
+import lexer.Token;
 import lexer.TokenType;
 
+import parser.Expression;
+
 public class Parser {
+
+        public static Expression parse(ArrayList<Token> tokens) {
+                
+                return null;
+        }
 }
 
