24ae655877c60a03c425e0fd10bc786a1fa7a301
Garrett Bates <garrett.bates@student.nmt.edu>
2020-02-06 12:49:47 -0700

    Added current and previous members to Parser to keep track of tokens.

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -9,7 +9,15 @@ import parser.Expression;
 
 public class Parser {
 
+        static Token previous;
+        static Token current;
+        static ArrayList<Token> tokens;
+
         public static Expression parse(ArrayList<Token> tokens) {
+
+                Parser.tokens = tokens;
+                Parser.current = tokens.remove(0);
+                Parser.previous = Parser.current;
                 
                 return null;
         }
