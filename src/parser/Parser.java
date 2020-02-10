1c69f5e1ff0172f36cb4aa27919f6990458ad4ec
Garrett Bates <garrett.bates@student.nmt.edu>
2020-02-09 22:39:19 -0700

    Added comment pertaining to parse table

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -24,6 +24,24 @@ public class Parser {
                 PRIMARY
         }
 
+        /*
+         * 09 Feb 2020
+         *
+         * I know I made this commit, but I've done some investigating
+         * since then, so I wanted to leave a little comment so that
+         * everyone else can weigh in their thoughts on how this
+         * parse table should be done.
+         *
+         * It might be easier (if somewhat hackish) to make ParseInfix
+         * and ParsePrefix into members of type Runnable.
+         * Otherwise we will have to have a subclass for every
+         * parse rule, and that could get pretty hairy.
+         *
+         * See this example here:
+         * https://programming.guide/java/function-pointers-in-java.html
+         *
+         * -- Garrett
+         */
         static class ParseRule {
 
                 public abstract void ParseInfix();
