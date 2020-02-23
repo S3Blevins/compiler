e9b113eecd1e8658f77e9da04b945c14098a3beb
Jacob Santillanes <santillanesjacob@gmail.com>
2020-02-22 21:21:19 -0700

    Added methods adding variables to table

--- src/parser/SymbolTable.java
+++ src/parser/SymbolTable.java
@@ -1,6 +1,8 @@
 package parser;
 //TODO add more Nessasary functions, implement in parser
 
+import lexer.Token;
+import parser.treeObjects.Declaration;
 import parser.treeObjects.treeList;
 
 import java.util.ArrayList;
@@ -8,13 +10,17 @@ import java.util.HashMap;
 
 public class SymbolTable {
     //Create a new HasMap <Type, ID>
-    HashMap<String, String> ST = new HashMap<String, String>();
+    HashMap<Token, Token> ST;
 
     //Creates a new array of hash maps
     ArrayList<SymbolTable> children;
 
+    public SymbolTable(){
+        this.ST = new HashMap<Token,Token>();
+    }
+
     //create a new hash map
-    public void addChild(SymbolTable child) {
+    public void addChildTable(SymbolTable child) {
         if(this.children == null) {
             this.children = new ArrayList<>();
         }
@@ -22,27 +28,36 @@ public class SymbolTable {
     }
 
     //add a new element to current hash map
-    public void addSymbol(String type, String ID){
-        ST.put(type, ID);
+    public void addSymbol(Token type, Token ID){
+        this.ST.put(type, ID);
+    }
+
+    public void addSymbol(Declaration.varDeclaration varDec){
+        Declaration.Variable var = varDec.getVarDec();
+
+        Token type = var.getType();
+        Token ID = var.getVariableID();
+        this.ST.put(type, ID);
     }
 
     //retreve a specific element
-    public String hasSymbol(String ID){
-        return ST.get(ID);
+    public Token hasSymbol(Token ID){
+        return this.ST.get(ID);
     }
 
-    public int childSize() {
+    public int childTableSize() {
         if(this.children == null) {
             return 0;
         }
-        return children.size();
+        return this.children.size();
     }
 
-    public boolean hasChildren() {
+    public boolean hasTableChildren() {
         if(this.children == null) {
             return false;
         }
         return true;
     }
+
 
 }
