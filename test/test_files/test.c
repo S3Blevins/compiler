019aedadd3652dcc75a6c6e5b164ec32ce433314
S3Blevins <s3.blevins@gmail.com>
2020-02-25 13:29:11 -0700

    Adding a more up to date test file.

--- test/test_files/test.c
+++ test/test_files/test.c
@@ -1,3 +1,41 @@
-int main() {
+
+/* test */
+int main(int h, int f) {
+
+    // expression testing
+    //(1 + 2);
+
+    // variable declaration testing
+    int x;
+    int x = 7;
+    int x, y = 3;
+    int x = 10, y = 3;
+
+    int a, b, c;
+
+    // iterative statement testing
+    while(1) {
+        int l;
+    }
+
+    // condition statement testing
+    if(4) {
+        while(1+4) {
+            // break statement testing
+            break;
+            int e;
+        }
+    } else if (9) {
+        int w;
+    } else {
+        int h;
+    }
+
+    // return statement testing
     return 1;
+}
+
+// another function testing
+int foo(int n) {
+    int g;
 }
