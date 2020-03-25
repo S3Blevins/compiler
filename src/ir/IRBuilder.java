a56a65a2a1aa01e7de771a761ba7107e2a1a0a27
Damon Estrada <damon.estrada@student.nmt.edu>
2020-03-24 20:36:32 -0600

    A start to handling IR creation.

--- src/ir/IRBuilder.java
+++ src/ir/IRBuilder.java
@@ -1,10 +1,26 @@
 package ir;
 
 import common.IVisitor;
+import lexer.Token;
 import parser.treeObjects.*;
 
+/**
+ * This class handles all of the routines and details of
+ * 1) Creating an IR object (IRExpression.java).
+ * 2) Populating that object.
+ * 3) Returning all of the objects to update IRList object (IRList.java)
+ *    with all of the crafted IRs.
+ */
 public class IRBuilder implements IVisitor<IRExpression> {
+        /*
+                        +
+                       /  \
+                      3    5
 
+                      LOAD 3, _1
+                      LOAD 5, _2
+                      ADD, _1, _2, L0
+         */
         @Override
         public IRExpression visitUnary(Expression.Unary unary) {
                 return null;
@@ -12,6 +28,20 @@ public class IRBuilder implements IVisitor<IRExpression> {
 
         @Override
         public IRExpression visitBinary(Expression.Binary binary) {
+                System.out.println("Visit Binary");
+
+                IRExpression left = binary.getLeftExpr().accept(this);
+                IRExpression right = binary.getRightExpr().accept(this);
+
+                IRList IRS = new IRList();
+                Token IRVarName = new Token(IRS.getLabelName());
+
+                IRExpression result = new IRExpression(Instruction.ADD, left.dest, right.dest, IRVarName);
+                //result.source0 = binary.right.token;
+                //result.source1 = binary.left.token;
+                //result.label_name = left.label_name;
+                //result.label_id = left.label_id + 1;
+
                 return null;
         }
 
@@ -27,6 +57,8 @@ public class IRBuilder implements IVisitor<IRExpression> {
 
         @Override
         public IRExpression visitNumber(Expression.Number number) {
+
+                System.out.println("Here");
                 return null;
         }
 
@@ -77,6 +109,11 @@ public class IRBuilder implements IVisitor<IRExpression> {
 
         @Override
         public IRExpression visitVarDecl(Declaration.varDeclaration decl) {
+
+                for (int i = 0; i < decl.children.size(); i++) {
+                        decl.children.get(i).accept(this);
+                }
+
                 return null;
         }
 
@@ -87,7 +124,11 @@ public class IRBuilder implements IVisitor<IRExpression> {
 
         @Override
         public IRExpression visitVariable(Declaration.Variable decl) {
-                return null;
+
+                IRExpression tmp = decl.children.get(0).accept(this);
+                Token var = tmp.dest;
+
+                return new IRExpression(Instruction.LOAD, var);
         }
 
         @Override
@@ -112,6 +153,14 @@ public class IRBuilder implements IVisitor<IRExpression> {
 
         @Override
         public IRExpression visitProgram(Program program) {
-                return null;
+                System.out.println("Visit Program");
+
+                for (int i = 0; i < program.children.size(); i++) {
+                        program.children.get(i).accept(this);
+                }
+
+                //for (int i = 0; i < program)
+
+                return null; // needs to return IR
         }
 }
