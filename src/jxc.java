8197405a8e2815e7294793937ea9a67b604107bc
Damon Estrada <damon.estrada@student.nmt.edu>
2020-03-24 20:35:40 -0600

    Added entry point when compiled with no optimization.

--- src/jxc.java
+++ src/jxc.java
@@ -1,3 +1,4 @@
+import ir.IRBuilder;
 import lexer.Lexer;
 import lexer.Token;
 import org.apache.commons.cli.*;
@@ -117,16 +118,12 @@ public class jxc {
             //display parse tree to command line
             System.out.println("\n\nPARSER:");
 
-            NodePrinter printer = new NodePrinter();
-            root.accept(printer);
-
             //NodePrinter printer = new NodePrinter();
             //root.accept(printer);
         }
 
         if (line.hasOption("po")) {
             //prints parse tree to output file
-
         }
 
         if (line.hasOption("s")) {
@@ -137,6 +134,8 @@ public class jxc {
         if (line.hasOption("O0")) {
             System.out.println("compile with no optimization");
 
+            IRBuilder irBuilder = new IRBuilder();
+            root.accept(irBuilder);
         }
 
         if (line.hasOption("O1")) {
