fbd6a8a10f29389231efd6cbe186daf7801b81d9
Damon Estrada <damon.estrada@student.nmt.edu>
2020-03-24 20:38:02 -0600

    The wrapper class to hold all of the Intermediate representations.

--- src/ir/IRList.java
+++ src/ir/IRList.java
@@ -1,5 +1,25 @@
 package ir;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The wrapper class that will house all of the IR
+ * in an arraylist.
+ */
 public class IRList {
 
+    public List<IRExpression> IRs = new ArrayList<>();
+
+    public String label_name;
+    public int label_id;
+
+    public IRList() {
+        label_name = "L";
+        label_id = 0;
+    }
+
+    String getLabelName() {
+        return label_name + label_id;
+    }
 }
