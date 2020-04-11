889a04a11210fbebd40c7aefcb734ad0535a0157
Damon Estrada <damon.estrada@student.nmt.edu>
2020-04-10 20:55:49 -0600

    Restructured test file to show what we support and how to format it.

--- test/test_files/test.c
+++ test/test_files/test.c
@@ -12,24 +12,25 @@ typedef enum {
     SUN
 } weekend_type;
 
+// Global variables
 int tmp;
+int length = 4, width, height;
 
-// another function testing
-int foo(int p) {
-    int q;
+// Define functions above other functions call them.
+int add(int num1, int num2) {
+    return num1 + num2;
 }
 
 int conditionals(int x, int y) {
-
     int j = 2;
 
-    if(x < 3) {
+    if (x < 3) {
         return 1;
     } else if(y < 4) {
         if(j < 9) {
             j++;
         }
-        return 7;
+        return j;
     } else {
         return 9;
     }
@@ -37,19 +38,17 @@ int conditionals(int x, int y) {
 
 int main(int a, int b, int y) {
 
-    //int test = 8 + (9 - 2 + 6);
-
-    //int zdfjhsaldkfjhsdlkafhj;
-
-    //function(7, 6);
+    int test = 8 + (9 - 2 + 6);
 
     int sum;
 
-    sum = foo(8, 9, 8, 89, 7, 0, 5, 5);
+    // Expression testing
+    sum = add(8, 9, 8, 89, 7, 0, 5, 5);
 
     a = (b * 3) - 2 + 10;
     a++;
 
+    // Goto label
     label:
 
     int x = 1;
@@ -59,23 +58,37 @@ int main(int a, int b, int y) {
     !x;
     ++x + y;
     x++ + y;
-    // expression testing
+
     (1 + 2 - -3 / 6);
     x = 3;
 
-    if(a != b) {
-        a *= b;
-        b /= a;
-        b -= a;
-        a += b;
+    // Conditional & assignment operator testing
+    if (a != b) {
+        a *= 5;
+        b /= 10;
+        b -= 20;
+        a += 45;
     }
 
+    if(4) {
+        while(1+4) {
+             // break statement testing
+             break;
+             int m;
+        }
 
-    for(int i; i < y; i++) {
+        if (9) {
+            int n;
+        } else {
+            int o;
+        }
+    }
+
+    // For loop testing
+    for (int i; a < y; y++) {
         int h = 9;
-        (a > h);
 
-        for(int p = 1; p < 8; p++) {
+        for (int p = 1; p < 8; p++) {
             p++;
         }
     }
@@ -98,18 +111,21 @@ int main(int a, int b, int y) {
 
     for(;;) {
         y - 9;
+        break;
     }
 
+    // Expression testing
     (1 == 2 > 3);
     (1 < 3);
     (1 > 4);
     (1 >= 3);
     (1 <= 2);
 
-    goto label;
-
     (1 > 2 && 0 < 1);
     (1 > 2 || 0 < 1);
+
+    // goto call
+    goto label;
 
     // variable declaration testing
     int c;
@@ -119,32 +135,15 @@ int main(int a, int b, int y) {
 
     int q, j, k;
 
-    // iterative statement testing
+    // while loop testing
     while(1) {
-        int l;
+        j++;
     }
 
-    // condition statement testing
-    if(4) {
-        while(1+4) {
-            // break statement testing
-            break;
-            int m;
-        }
-    } else if (9) {
-        int n;
-    } else {
-        int o;
+    while(true) {
+        q++;
     }
 
     // return statement testing
     return 1;
 }
-
-int foobar() {
-
-	int p;
-	return 0;
-}
-
-
