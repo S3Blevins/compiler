23f73982aceff673b6010f68d054958365ec1602
Garrett Bates <garrett.bates@student.nmt.edu>
2020-04-18 19:51:27 -0600

    Added some changes to AsmGenerator -- a lot of failed attempts at getting optimization to work, and this is all I have that I didn't obliterate.

--- src/asm/AsmGenerator.java
+++ src/asm/AsmGenerator.java
@@ -2,15 +2,44 @@ package asm;
 
 import ir.IRExpression;
 import ir.IRList;
+import ir.Instruction;
+import lexer.Token;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class AsmGenerator {
 
     private static AsmGenerator instance;
+    /*private class IRRef {
+
+        private final IRExpression expr;
+        private final int location;
+
+        public IRRef(IRExpression expr, int location) {
+
+            this.expr = expr;
+            this.location = location;
+        }
+
+        public IRExpression getExpr() {
+            return expr;
+        }
+
+        public int getLocation() {
+            return location;
+        }
+    }
+
+    ArrayList<IRRef> constants;*/
+
+    HashMap<Token, Integer> constants;
 
     private AsmGenerator() {
+
+        //constants = new ArrayList<>();
+        constants = new HashMap<>();
     }
 
     static AsmGenerator getInstance() {
@@ -37,5 +66,44 @@ public class AsmGenerator {
         //  .;::::;.
         //   .;::;.
         //     ..
+
+        for(int i = 0; i < irExprList.size(); i++) {
+
+            if(irExprList.get(i).inst == Instruction.LOAD) {
+
+                constants.putIfAbsent(irExprList.get(i).dest, i);
+            }
+        }
+
+        for(int i = 0; i < irExprList.size(); i++) {
+
+            switch(irExprList.get(i).inst) {
+
+                case ADD:
+                case SUB:
+                case MUL:
+                case DIV:
+                case NOT: {
+
+                    Token s0 = irExprList.get(i).sources.get(0);
+                    Token s1 = irExprList.get(i).sources.get(1);
+                    Token d = irExprList.get(i).dest;
+
+                    if(constants.containsKey(s0) && constants.containsKey(s1)) {
+
+                        int s0_index = constants.get(s0);
+                        int s1_index = constants.get(s1);
+
+                        // This will break how the loop works..
+                        // FFFFFFFUUUU ---
+                        irExprList.remove(s0_index);
+                        irExprList.remove(s1_index);
+                    }
+
+                } break;
+                default:
+                    break;
+            }
+        }
     }
 }
