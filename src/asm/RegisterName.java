423c69c67fe7102378144daa06756d84651f6ece
Damon Estrada <damon.estrada@student.nmt.edu>
2020-04-21 13:59:40 -0600

    Changed the class name from register to RegisterName. Changed all of the registers to 32bit from 64bit.

---
+++ src/asm/RegisterName.java
@@ -0,0 +1,20 @@
+package asm;
+
+public enum RegisterName {
+        eax,
+        ebx,
+        ecx,
+        edx,
+        edi,
+        esi,
+        ebp,
+        esp,
+        r8,
+        r9,
+        r10,
+        r11,
+        r12,
+        r13,
+        r14,
+        r15
+}
