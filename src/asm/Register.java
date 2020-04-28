a588cecde063a25737b972f924370e347aa37003
S3Blevins <s3.blevins@gmail.com>
2020-04-28 12:41:37 -0600

    Added missing file.

---
+++ src/asm/Register.java
@@ -0,0 +1,20 @@
+package asm;
+
+public enum Register {
+        edi,
+        esi,
+        edx,
+        ecx,
+        r8d,
+        r9d,
+        eax,
+        ebx,
+        //rbp - reserved
+        //rsp - reserved
+        r10d,
+        r11d,
+        r12d,
+        r13d,
+        r14d,
+        r15d
+}
